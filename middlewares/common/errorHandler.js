defcead8448328a0af34ebc5597e0fb7916bd69c
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-05-01 19:07:47 +0600

    error handler

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -9,8 +9,19 @@ function notFoundHandler(req, res, next) {
 // default error handler middleware
 
 function errorHandler(err, req, res, next) {
-    res.locals.title = 'Error Page';
-    res.render('error')
+    res.locals.error = process.env.NODE_ENV === 'development' ? err : { message: err.message };
+    res.status(err.status || 500);
+
+    if (res.locals.html) {
+        // html response
+        res.render('error', {
+            title: 'Error Page',
+        })
+    } else {
+        // json response
+        res.json(res.locals.error);
+    }
+
     // res.render('error', {
     //     title: 'Error Page',
     // })
